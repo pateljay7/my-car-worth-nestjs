6d2474bd5ac41cb1b572d4bdf3054828bfeee6e1
pateljay7 <pateljaykjp@gmail.com>
2024-02-23 11:33:25 +0530

    feat: created report entity and create-report dto file. implemented POST/GET api for report.

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+  @Post()
+  createReport(@Body() body: CreateReportDto) {
+    return this.reportsService.create(body);
+  }
+
+  @Get()
+  getAllReports() {
+    return this.reportsService.find();
+  }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from 'src/database/entities/report.entity';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  create(body: CreateReportDto) {
+    try {
+      const report = this.repo.create(body);
+      return this.repo.save(report);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async find() {
+    try {
+      return this.repo.find();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
