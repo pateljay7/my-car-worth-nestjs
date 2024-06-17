1df10ab58b31a01bfdfc7e38bfb7fedb46a438b5
pateljay7 <pateljaykjp@gmail.com>
2024-06-17 12:40:29 +0530

    fix: resolve user type issue in request interface for current-user-middleware.

--- src/core/middlewares/current-user-middleware.ts
+++ src/core/middlewares/current-user-middleware.ts
@@ -1,10 +1,25 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { User } from 'src/database/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
+/* 
+  - Request object does't have user property
+  - As we are adding user property by our end in JWT strategy
+  - Need to update Request interface to use user property
+ */
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      user?: User;
+    }
+  }
+}
 //Middlware will call top of the JWT strategy and authGuard
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
   constructor(private usersService: UsersService) {}
-  async use(req: any, res: any, next: (error?: any) => void) {
+  async use(req: Request, res: Response, next: NextFunction) {
     const { id } = req.user || {};
 
     if (id) {
