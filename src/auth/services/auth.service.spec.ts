2802dfc785c4a6220e5629971cfcfabf77d452c6
pateljay7 <pateljaykjp@gmail.com>
2024-06-21 10:32:12 +0530

    Test: change - fakeUsersService method implementation.

--- src/auth/services/auth.service.spec.ts
+++ src/auth/services/auth.service.spec.ts
@@ -10,10 +10,23 @@ describe('AuthService', () => {
   let fakeUsersService: Partial<UsersService>;
   beforeEach(async () => {
     //Create a fake copy of the usersService
+    const users: User[] = [];
     fakeUsersService = {
-      find: () => Promise.resolve([]),
-      create: ({ email, password }) =>
-        Promise.resolve({ id: 1, email, password } as User),
+      find: (email: string) => {
+        const filteredUsers = users.filter((user) => user.email === email);
+        return Promise.resolve(filteredUsers);
+      },
+      create: ({ email, password, first_name, last_name }) => {
+        const user = {
+          id: Math.floor(Math.random() * 99999),
+          email,
+          password,
+          first_name,
+          last_name,
+        } as User;
+        users.push(user);
+        return Promise.resolve(user);
+      },
     };
     const fakeJwtService = {
       generateToken: () => '',
@@ -53,16 +66,12 @@ describe('AuthService', () => {
   });
 
   it('throws an error if user signs up with existing email', async () => {
-    fakeUsersService.find = () =>
-      Promise.resolve([
-        {
-          id: 1,
-          email: 'jay@gmail.com',
-          password: 'jay@123',
-          first_name: 'jay',
-          last_name: 'Patel',
-        } as User,
-      ]);
+    await service.signup({
+      email: 'jay@gmail.com',
+      password: 'jay@123',
+      first_name: 'jay',
+      last_name: 'Patel',
+    });
 
     await expect(
       service.signup({
@@ -95,38 +104,28 @@ describe('AuthService', () => {
   });
 
   it('throws if an invalid password is provided', async () => {
-    fakeUsersService.find = () =>
-      Promise.resolve([
-        {
-          id: 1,
-          email: 'jay@gmail.com',
-          password: 'jay@123',
-          first_name: 'jay',
-          last_name: 'Patel',
-        } as User,
-      ]);
+    await service.signup({
+      email: 'jay@gmail.com',
+      password: 'jay@123',
+      first_name: 'jay',
+      last_name: 'Patel',
+    });
 
     await expect(
       service.login({
-        email: 'jay@gmail.com',
-        password: 'kajnkdad',
+        email: 'jaypatel@gmail.com',
+        password: 'jay@123',
       }),
     ).rejects.toThrow(new BadRequestException('Invalid credential'));
   });
 
   it('returns a user if correct password', async () => {
-    fakeUsersService.find = () =>
-      Promise.resolve([
-        {
-          id: 1,
-          email: 'jay@gmail.com',
-          password:
-            '$2b$10$Wm5GWcwxHQab9u3IGGGwY.FLXef/MBc3t8yHLD.MEnxuUunGs0S4O',
-          first_name: 'jay',
-          last_name: 'Patel',
-        } as User,
-      ]);
-
+    await service.signup({
+      email: 'jay@gmail.com',
+      password: 'jay@123',
+      first_name: 'jay',
+      last_name: 'Patel',
+    });
     const user = await service.login({
       email: 'jay@gmail.com',
       password: 'jay@123',
